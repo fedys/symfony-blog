399ef90cdb6d3b79e3790cb33e439df73269c904
Miroslav Fedeleš <miroslav.fedeles@gmail.com>
2019-09-19 19:59:14 +0200

    Frontend

--- src/src/Controller/Admin/SecurityController.php
+++ src/src/Controller/Admin/SecurityController.php
@@ -4,7 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Form\LoginType;
 use LogicException;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 /**
  * @Route(name="security_")
  */
-class SecurityController extends Controller
+class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
